3b1518cc4cb49687c661fb4382beb92c518242d7
jitendra_bairwa <jitendrabairwa225@gmail.com>
2024-02-16 00:32:45 +0530

    local storage work done

--- main.js
+++ main.js
@@ -6,26 +6,23 @@ let notesDisplay = document.querySelector(".notes-display");
 let showPinnedNotes = document.querySelector(".pinned-notes-container");
 let showOtherNotes = document.querySelector(".notes-container");
 
-let arrayOfNotes = [{
-    title: "todo",
-    note: "ASDFGHJKL",
-    _id: "123",
-    isPinned: false,
-    isArchived: false
-}]
+let arrayOfNotes = JSON.parse(localStorage.getItem('notes')) || []   // JSON.parse is used to convert string into object in local storage.
 
-addNoteButton.addEventListener('click',()=>{
+addNoteButton.addEventListener('click', () => {
     // console.log(title.value);
     // console.log(note.value);
 
-    if(title.value.trim().length>0 || note.value.trim().length>0){
-        arrayOfNotes = [...arrayOfNotes,{title:title.value.trim(),note:note.value.trim(),_id:Date.now(),isPinned:false,isArchived:false}];
+    if (title.value.trim().length > 0 || note.value.trim().length > 0) {
+        arrayOfNotes = [...arrayOfNotes, { title: title.value.trim(), note: note.value.trim(), _id: Date.now(), isPinned: false, isArchived: false }];
         // console.log(arrayOfNotes);
 
         title.value = note.value = "";
         showOtherNotes.innerHTML = renderNotes(arrayOfNotes);
-
+        localStorage.setItem("notes", JSON.stringify(arrayOfNotes));   // JSON.stringfy is used to converted object into string in local storage.
     }
-
 })
+showOtherNotes.innerHTML = renderNotes(arrayOfNotes);
+
+
+
 
