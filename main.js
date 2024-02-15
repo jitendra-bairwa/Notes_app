3d508d4e666b024b331a9cde45fc2af4a8189cf4
jitendra_bairwa <jitendrabairwa225@gmail.com>
2024-02-15 23:30:46 +0530

    Beginning

---
+++ main.js
@@ -0,0 +1,22 @@
+let title = document.querySelector(".title");
+let note = document.querySelector(".note");
+let addNoteButton = document.querySelector(".add-btn");
+let notesDisplay = document.querySelector(".notes-display");
+let showPinnedNotes = document.querySelector(".pinned-notes-container");
+let showOtherNotes = document.querySelector(".notes-container");
+
+let arrayOfNotes = [{
+    title: "todo",
+    note: "ASDFGHJKL",
+    _id: "123",
+    isPinned: false,
+    isArchived: false
+}]
+
+addNoteButton.addEventListener('click',()=>{
+    // console.log(title.value);
+    // console.log(note.value);
+
+    
+})
+
